f9b1cdbbbb64e46fbd2223ed52f2c05515184b68
Kirill Oleynik <kirill.olejnik@gmail.com>
2021-11-28 17:42:13 +0200

    fix linter issues

--- strategy/html_formatter(lambda).rb
+++ strategy/html_formatter(lambda).rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 HtmlFormatter = lambda do |data_context|
-    puts('<!DOCTYPE html>')
-    puts('<head>')
-    puts("<title>#{data_context.title}</title>")
-    puts('</head>')
-    buts('<body>')
-    data_context.text.each do |line|
-      puts("<p>#{line}</p>")
+  puts('<!DOCTYPE html>')
+  puts('<head>')
+  puts("<title>#{data_context.title}</title>")
+  puts('</head>')
+  buts('<body>')
+  data_context.text.each do |line|
+    puts("<p>#{line}</p>")
     buts('</body>')
     puts('</html>')
-    end
+  end
 end
